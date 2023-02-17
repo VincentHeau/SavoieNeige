8f568c4af0663a7c6d2181b566638c4dbc5fca7c
MatthieuAhr <matthieu.ahr@ensg.eu>
2023-02-17 16:30:14 +0100

    tentative de style bouton et de slider

--- js/main.js
+++ js/main.js
@@ -306,3 +306,15 @@ labelsPois.selectAll("text")
 	// Création dictionnaire des stations avec hauteurs de neige
 
 
+// Partie JS du style du slider
+
+// var rangeSlider = document.getElementById("rs-range-line");
+// var rangeBullet = document.getElementById("rs-bullet");
+
+// rangeSlider.addEventListener("input", showSliderValue, false);
+
+// function showSliderValue() {
+//   rangeBullet.innerHTML = rangeSlider.value;
+//   var bulletPosition = (rangeSlider.value /rangeSlider.max);
+//   rangeBullet.style.left = (bulletPosition * 578) + "px";
+// }
